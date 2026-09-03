2419609a1f61961565035321d16b7347afa6ab9a
Eduardo <eduardosantos17007@outlook.com>
2026-09-03 17:58:18 -0300

    debbuging

--- src/client.py
+++ src/client.py
@@ -31,6 +31,15 @@ def get_matches(date, api_key=None):
         response.raise_for_status()
         return response.json()
 
+    except requests.HTTPError as error:
+        logger.exception(
+            "Erro HTTP %s ao buscar jogos para %s na API %s. Resposta: %s",
+            error.response.status_code,
+            date,
+            API_URL,
+            error.response.text,
+        )
+        raise
     except (requests.RequestException, ValueError):
         logger.exception("Erro ao buscar jogos para %s na API %s", date, API_URL)
         raise
